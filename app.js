71463ec4acbdfea87aaec25f7ee3c9d4e8ce0925
FaraahIbarra <47908177+FaraahIbarra@users.noreply.github.com>
2019-04-16 14:12:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,136 @@
-$('#start').on('click', function () {
-    game.start();
-})
-
-var questions = [{
-    question: "Which of these sea creatures is most dangerous?",
-    options: ["Blue-ringed Octopus", "Great White Shark", "Box Jellyfish", "Pufferfish"],
-    correctAnswer: "Box Jellyfish"
-},
-{
-    question: "An Electric Eel can power 10 lightbulbs with its electricity?",
-    options: ["True", "False"],
-    correctAnswer: "True"
-},
-{
-    question: "Which of these sea animals can change its gender?",
-    options: ["Clownfish", "Sea Turtle", "Dolphin", "Sea Horse"],
-    correctAnswer: "Clownfish"
-},
-{
-    question: "Which of these animals has 3 hearts and blue blood?",
-    options: ["Whale", "Tuna Fish", "Octopus", "Stingray"],
-    correctAnswer: "Octopus"
-},
-{
-    question: "The ocean's fastest animal is...",
-    options: ["Swordfish", "Mako Shark", "Killer Whale", "Sailfish"],
-    correctAnswer: "Sailfish"
-},
-{
-    question: "What is the deepest living fish in the sea?",
-    options: ["Mariana Snailfish", "Sunfish", "Barreleye Fish", "Squid"],
-    correctAnswer: "Mariana Snailfish"
-},
-{
-    question: "When dolphins sleep only 50% of their brain shuts down and one eye closes.",
-    options: ["True", "False"],
-    correctAnswer: "True"
-},
-{
-    question: "Which of these is not true about sea otters?",
-    options: ["Their favorite snack is the Sea Urchin", "They keep warm with their blubber", "They hold hands while they sleep", "There are 13 otter species in the world"],
-    correctAnswer: "They keep warm with their blubber"
-}
-}];
+$(document).ready(function () {
+});
 
 var game = {
-    correct: 0,
-    incorrect: 0,
-    counter: 30,
-    countdown: function () 
+    questions: [
+        {
+    id: 'question-one',
+    question: "Which of these sea creatures is most dangerous?",
+    choices: ["Blue-ringed Octopus", "Great White Shark", "Box Jellyfish", "Pufferfish"],
+    answer: "Box Jellyfish"
+},
+{
+    id: 'question-two',
+    question: "An Electric Eel can power 10 lightbulbs with its electricity?",
+    choices: ["True", "False"],
+    answer: "True"
+},
+{
+    id: 'question-three',
+    question: "Which of these sea animals can change its gender?",
+    choices: ["Clownfish", "Sea Turtle", "Dolphin", "Sea Horse"],
+    answer: "Clownfish"
+},
+{
+    id: 'question-four',
+    question: "The ocean's fastest animal is...",
+    choices: ["Swordfish", "Mako Shark", "Killer Whale", "Sailfish"],
+    answer: "Sailfish"
+},
+{
+    id: 'question-five',
+    question: "What is the deepest living fish in the sea?",
+    choices:["Mariana Snailfish", "Sunfish", "Barreleye Fish", "Squid"],
+    answer: "Mariana Snailfish"
+},
+{
+    id: 'question-six',
+    question: "Which of these is not true about sea otters?",
+    choices: ["Their favorite snack is the Sea Urchin", "They keep warm with their blubber", "They hold hands while they sleep", "There are 13 otter species in the world"],
+    answer: "They keep warm with their blubber"
+},
+]}
+
+$(".startGame").on("click", function () {
+
+var number = 60;
+$('#timeLeft').on('click', run);
+
+function decrement() {
+    number--;
+    $('#timeLeft').html('<h2>' + number + " seconds" + '</h2>');
+    if (number === 0) {
+        stop();
+        checkAnswers();
+    }
 }
+function run() {
+    counter = setInterval(decrement, 1000);
+}
+
+function stop() {
+    clearInterval(counter);
+}
+run();
+
+function formTemplate(data) {
+    var qString = "<form id='questionOne'>" + data.question + "<br>";
+    var choices = data.choices;
+    for (var i = 0; i < choices.length; i++) {
+        var choice = choices[i];
+        console.log(choice);
+        qString = qString + "<input type='radio' name='" + data.id + "' value=" + i + ">" + choice;
+
+    }
+    return qString + "</form>";
+}
+window.formTemplate = formTemplate;
+
+function buildQuestions() {
+    var questionHTML = ''
+    for (var i = 0; i < game.questions.length; i++) {
+        questionHTML = questionHTML + formTemplate(game.questions[i]);
+    }
+    $('#questions-container').append(questionHTML);
+
+}
+
+function isCorrect(question) {
+    var answers = $('[name=' + question.id + ']');
+    var correct = answers.eq(question.answer);
+    var isChecked = correct.is(':checked');
+    return isChecked;
+}
+
+buildQuestions();
+
+function resultsTemplate(question) {
+    var htmlBlock = '<div>'
+    htmlBlock = htmlBlock + question.question + ': ' + isChecked;
+    return htmlBlock + "</div>";
+}
+
+function checkAnswers() {
+    var correct = 0;
+    var incorrect = 0;
+    var unAnswered = 0
+
+    for (var i = 0; i < game.questions.length; i++) {
+        if (isCorrect(game.questions[i])) {
+            correct++;
+        } else {
+            if (checkAnswered(game.questions[i])) {
+                incorrect++;
+            } else {
+                unAnswered++;
+            }
+        }
+    }
+
+    $('.results').html('correct: ' + correct + "<br>" + 'incorrect: ' + incorrect + "<br>" + 'unanswered: ' + unAnswered);
+}
+
+function checkAnswered(question) {
+    var anyAnswered = false;
+    var answers = $('[name=' + question.id + ']');
+    for (var i = 0; i < answers.length; i++) {
+        if (answers[i].checked) {
+            anyAnswered = true;
+        }
+    }
+    return anyAnswered;
+}
+$('#doneButton').on('click', function () {
+    checkAnswers();
+    stop();
+})
